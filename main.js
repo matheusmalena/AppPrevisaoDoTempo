53743aafa47b616cd459602625c84f3b0b066432
Matheus Malena <matheusmalena28@gmail.com>
2023-10-05 17:45:23 -0300

    commit 0510

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ function CliqueiNoBotao() {
 
 async function buscarCidade(cidade) {
     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+    //dadosNaTela(dados)
     colocarDadosnaTela(dados)
 }
 
 function colocarDadosnaTela(dados) {
-    console.log(dados)
     document.querySelector(".titulo").innerHTML = "Tempo em " + dados.name
     document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "C°"
     document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
@@ -20,3 +20,14 @@ function colocarDadosnaTela(dados) {
     document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
 
 }
+
+/*function dadosNaTela(dados){
+    console.log(dados)
+    const dadosNaTela = document.getElementById("dadosNaTela")
+    document.querySelector(".titulo").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "C°"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    
+}*/
